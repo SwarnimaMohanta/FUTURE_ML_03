6493eea7ff3ff81173e021e1845e1534eafb8a0b
SwarnimaMohanta <mohantaswarnima448@gmail.com>
2025-07-27 19:41:33 +0800

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,57 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+  document.querySelectorAll('header nav button').forEach(button => {
+    button.addEventListener('click', function() {
+      const targetId = this.getAttribute('data-target');
+      const targetSection = document.querySelector(targetId);
+      if (targetSection) {
+        targetSection.scrollIntoView({ behavior: 'smooth' });
+      }
+    });
+  });
+})
+
+   window.downloadCV = function () {
+    const link = document.createElement('a');
+    link.href = 'assets/Swarnima_Mohanta_CV.pdf';
+    link.download = 'Swarnima_Mohanta_CV.pdf';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
+  const textElement = document.querySelector(".gradient-text");
+  const texts = ["AI/ML Developer", "Coder"];
+  let textIndex = 0;
+  let charIndex = 0;
+  let isDeleting = false;
+  const typingSpeed = 100; 
+  const deletingSpeed = 70; 
+  const pauseBeforeDelete = 1500; 
+  const pauseBeforeType = 500;
+
+  function typeWriter() {
+    const currentText = texts[textIndex];
+    if (isDeleting) {
+      textElement.textContent = currentText.substring(0, charIndex - 1);
+      charIndex--;
+    } else {
+      textElement.textContent = currentText.substring(0, charIndex + 1);
+      charIndex++;
+    }
+
+    if (!isDeleting && charIndex === currentText.length) {
+      setTimeout(() => isDeleting = true, pauseBeforeDelete);
+    } else if (isDeleting && charIndex === 0) {
+      isDeleting = false;
+      textIndex = (textIndex + 1) % texts.length;
+      setTimeout(() => {}, pauseBeforeType); 
+    }
+
+    const currentSpeed = isDeleting ? deletingSpeed : typingSpeed;
+    setTimeout(typeWriter, currentSpeed);
+  }
+
+  typeWriter(); 
+
+  
